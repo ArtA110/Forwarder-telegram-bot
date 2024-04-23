e93e52b56f5ba342389bb7ed20a953e80de778ad
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2024-04-23 05:13:33 +0330

    first version

--- telegrambot.py
+++ telegrambot.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+from collections import OrderedDict
 from typing import Final
 from hashlib import sha256
 from pymongo import MongoClient
@@ -15,12 +16,13 @@ logger = logging.getLogger(__name__)
 
 
 CHANNEL_ID: Final = -1001760329506
-TOKEN: Final = '6777553545:AAE_b9T8vcPbpGkm1pHKzsHqhfkrzfKTtAU'
+TOKEN: Final = '6777553545:AAFmGKGc1mqUORYJh9s4fRRaB4tXd7jJj8c'
 # Connect to database
 client = MongoClient('localhost', 27017)
 db = client['TelegramBot']
 users_collection = db['users']
 logged_in_collection = db['logged_in']
+files_collection = db['files']
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -29,33 +31,115 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text += '\n'.join(options)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [InlineKeyboardButton("Option 1", callback_data="1")],
-        [
-            InlineKeyboardButton("Option 2", callback_data="2"),
-            InlineKeyboardButton("Option 3", callback_data="3")],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Hey', reply_markup=reply_markup)
-
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-
-    await query.edit_message_text(text=f'you choose: {query.data}')
-
 USERNAME, PASSWORD = range(2)
 REGISTER = 0
+STATE0, STATE1, STATE2 = range(3)
+
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not is_authorized(update.effective_chat.id):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='ابتدا باید /login کنید')
+        return ConversationHandler.END
+
+    all_topics = {'اصول فقه': 'osool', 'تذکره': 'tazkare', 'دره نجفیه': 'dorre', 'رجوم الشیاطین': 'rojoom',
+                  'رساله غیبت': 'gheibat', 'سلطانیه': 'soltanieh', 'شرایط دعا': 'doa', 'قرآن محشی': 'mohassha',
+                  'متفرقه': 'others', 'معادیه': "ma'adieh", 'معرفت سر اختیار': 'marefat', 'منطق': 'mantegh',
+                  'مواعظ 1292': 'mavaez', 'میزان': 'mizan'}
+
+    response = create_menu(name_dict=all_topics)
+    reply_markup = InlineKeyboardMarkup(response)
+    context.user_data['query_item'] = []
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Test', reply_markup=reply_markup)
+    return STATE0
 
 
-def is_authorized(chat_id):
+async def button1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    context.user_data['query_item'].append(query.data)
+    search_result = sort_result(list(files_collection.find({"file_name": {"$regex": f"^{query.data}"}})))
+    sub_menu = {}
+    if len(search_result[1]['file_name'].split('-')) > 2:
+        all_topics = {'سوره بقره': 'baghareh', 'سوره حمد': 'hamd', 'سوره اخلاص': 'ekhlas', 'جزء سی': 'jozv30',
+                  'فخر رازی': 'FakhrRazi', 'تفتازانی': 'Taftazani', 'مقدمه': 'moghadame', 'جلد اول': '1',
+                  'جلد دوم': '2', 'سال 1992': '92'}
+        reverse_all_topic = {value: key for key, value in all_topics.items()}
+        for result in search_result:
+            if result['file_name'].split('-')[1].replace('.mp3', '') in all_topics.values():
+                sub_menu[reverse_all_topic[result['file_name'].split('-')[1].replace('.mp3', '')]] = result['file_name'].split('-')[1].replace('.mp3', '')
+    else:
+        for result in search_result:
+            sub_menu[result['title']] = result['file_name']
+    reply_markup = InlineKeyboardMarkup(create_menu(name_dict=sub_menu))
+    await query.edit_message_text(text=f'you choose: {query.data}', reply_markup=reply_markup)
+    return STATE1
+
+
+async def button2(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    # {"$regex": f"^{'-'.join(context.user_data['query_item'])}"}
+    context.user_data['query_item'].append(query.data)
+    if '.mp3' in query.data:
+        search_result = sort_result(list(files_collection.find({"file_name": {"$regex": f"^{query.data}"}})))
+    else:
+        search_result = sort_result(
+            list(files_collection.find({"file_name": {"$regex": f"^{'-'.join(context.user_data['query_item'])}"}})))
+    if len(search_result) == 1:
+        await update.effective_chat.forward_from(from_chat_id=CHANNEL_ID, message_id=search_result[0]['message_id'])
+        await query.edit_message_text(text='File Sent Successfully')
+        return ConversationHandler.END
+    sub_menu = {}
+    for result in search_result:
+        sub_menu[result['title']] = result['file_name']
+    reply_markup = InlineKeyboardMarkup(create_menu(name_dict=sub_menu))
+    await query.edit_message_text(text=f'you choose: {query.data}', reply_markup=reply_markup)
+    return STATE2
+
+
+
+async def button3(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    context.user_data['query_item'].append(query.data)
+    search_result = sort_result(
+        list(files_collection.find({"file_name": {"$regex": f"^{query.data}"}})))
+    await update.effective_chat.forward_from(from_chat_id=CHANNEL_ID, message_id=search_result[0]['message_id'])
+    await query.edit_message_text(text='File Sent Successfully')
+    return ConversationHandler.END
+
+def sort_result(result):
+    try:
+        return sorted(result, key=lambda x: int(x['file_name'].split('-')[-1].replace('.mp3', '')))
+    except ValueError:
+        return sorted(result, key=lambda x: x['file_name'].split('-')[-1].replace('.mp3', ''))
+
+
+def create_menu(count: int=None, name_dict: dict=None):
+    response = []
+    i = 0
+    if name_dict is not None:
+        for key, value in name_dict.items():
+            if i % 2 == 0:
+                response.append([InlineKeyboardButton(key, callback_data=value)])
+            else:
+                response[i // 2].append(InlineKeyboardButton(key, callback_data=value))
+            i+=1
+    elif count is not None:
+        for i in range(count):
+            if i % 2 == 0:
+                response.append([InlineKeyboardButton(str(i+1), callback_data=i)])
+            else:
+                response[i//2].append(InlineKeyboardButton(str(i+1), callback_data=i))
+    return response
+
+
+def is_authorized(chat_id: int) -> bool:
     if logged_in_collection.find_one({'chat_id': chat_id}):
         return True
     return False
+
+
 async def login_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if is_authorized(update.effective_user.id):
+    if is_authorized(update.effective_chat.id):
         text = 'شما قبلا وارد شدید و میتوانید از امکانات استفاده کنید.'
         await context.bot.send_message(chat_id=update.effective_chat.id, text=text,
                                        reply_to_message_id=update.effective_message.message_id)
@@ -107,7 +191,7 @@ async def login_cancel_handler(update: Update, context: ContextTypes.DEFAULT_TYP
 
 
 async def logout_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if is_authorized(update.effective_user.id):
+    if is_authorized(update.effective_chat.id):
         logged_in_collection.delete_one({'chat_id': update.effective_chat.id})
         await context.bot.send_message(chat_id=update.effective_chat.id, text='خارج شدید',
                                        reply_to_message_id=update.effective_message.id)
@@ -182,11 +266,18 @@ if __name__ == '__main__':
         states={TICKET: [MessageHandler(filters.TEXT & ~ filters.COMMAND, ticket_info_handler)]},
         fallbacks=[CommandHandler('cancel', login_cancel_handler)]
     )
+
+    audio = ConversationHandler(
+        entry_points=[CommandHandler('test', test)],
+        states={STATE0: [CallbackQueryHandler(button1)],
+                STATE1: [CallbackQueryHandler(button2)],
+                STATE2: [CallbackQueryHandler(button3)]},
+        fallbacks=[CommandHandler('cancel', login_cancel_handler)]
+    )
     application.add_handler(login)
     application.add_handler(register)
     application.add_handler(ticket)
+    application.add_handler(audio)
     application.add_handler(CommandHandler('logout', logout_handler))
     application.add_handler(CommandHandler('forward', forward_handler))
-    application.add_handler(CommandHandler('test', test))
-    application.add_handler(CallbackQueryHandler(button))
     application.run_polling()
